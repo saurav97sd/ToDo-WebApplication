efb69cd891a9df4d72ebc0e8f3d179a439dde762
Saurav Das <sauravdas969.sd@gmail.com>
2021-02-09 18:16:27 +0530

    Fetch data from MongoDB

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,14 +1,27 @@
-var todo = require("./mytemparr");
+// Require the temp array from ram
+// var todo = require("./mytemparr");
 
-//module.exports.actionname = function(req,res){}
+//-----------------------Adding the ODM---------------------------------------
+const db = require('../configs/mongoose');
+//-----------------------adding the schema------------------------------------
+const TD = require('../models/todo');
+
+//Rendering the home page
 module.exports.home = function(req,res){
     //return res.send('<h1>Express Up for CodeToDo</h1>');
 
-    //rendering the home.ejs file
-    return res.render('home', {
-        title : 'TODO WebApp',
-        todo : todo.todo
-    });
+    TD.find({}, function(err, allTodo){
+        if(err){
+            console.log('Error in featching');
+            return;
+        }
 
+        //rendering the home.ejs file
+        return res.render('home', {
+            title : 'TODO WebApp',
+            // todo : todo.todo
+            todo : allTodo
+        });
+    });
 };
 
