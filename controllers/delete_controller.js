6f7dda6c8223e518d80315579bdd308b38db8a00
Saurav Das <sauravdas969.sd@gmail.com>
2021-02-15 15:34:54 +0530

    delete multiple todo fail

--- controllers/delete_controller.js
+++ controllers/delete_controller.js
@@ -7,12 +7,13 @@ const TD = require('../models/todo');
 module.exports.todo = function(req, res){
     //return res.send('<h1>User Profile</h1>');
     // return res.redirect('back');
-    var items=[];
+    var items= new Array();
     console.log(req.body);
     for(var key in req.body){
         items=req.body[key];
     }
     console.log(items);
+    // TD.collection.deleteMany({'_id':{'$in':items}});
     // for(let i of items){
         let id = items;
         TD.findByIdAndDelete(id, function(err){
